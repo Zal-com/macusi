a94dd9cbd96e5bda610ca96f32268ad3702ceb38
Zal-com <st.guillaume@outlook.com>
2023-07-14 11:28:35 +0200

    idk

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,27 +1,58 @@
-@props(['mot'])
-<div class="border border-1 border-dark rounded w-25 mx-1 my-3 bg-black bg-opacity-10">
-    <div>
-        <h2 class="text-center py-4 border-bottom text-primary bg-white border-top rounded">{{ $mot->enMacusi }}</h2>
-    </div>
-    <div class="px-4 lh-2 ">
-        @php
-            $locale = (string) strtoupper(app()->getLocale());
-        @endphp
-        {{-- Traduction --}}
-        <p class="fw-bold lead text-center">{{json_decode($mot->trads)->$locale}}</p>
+@props(['mot', 'count'])
+@php
+    $locale = (string) strtoupper(app()->getLocale());
+@endphp
+<div id="{{'heading' . $count}}" class="w-40 my-2 mx-2">
+    <button class="btn border-secondary rounded-4 w-100 text-left" data-bs-toggle="collapse" data-bs-target="{{'#collapse' . $count}}" aria-expanded="true" aria-controls="{{'collapse' . $count}}">
+        <div id="header" class="align-middle font-weight-bold">
+            <img src="{{Storage::url('img/icon-comment.svg')}}" class="mx-2"> {{json_decode($mot->trads)->$locale}} : {{$mot->enMacusi}} <span class="float-right align-middle"><img src="{{Storage::url('img/icon-arrow.svg')}}"></span>
+        </div>
+        <div id="{{'collapse' . $count}}" class="collapse hide" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+                @if($mot->types->count() == 1)
+                    <h5>Type</h5>
+                    <hr class="hr">
+                    <p>{{ $mot->typesString() }} </p>
+                @else
+                    <h5>Types</h5>
+                    <hr class="hr">
+                    <p>{{ $mot->typesString() }} </p>
+                @endif
 
-        <p>
-        {{-- Listing of type(s) --}}
-        @if($mot->types->count() == 1)
-            <p>Type : {{ $mot->typesString() }} </p>
-        @elseif($mot->types->count() == 2)
-            <p>Types : {{ $mot->typesString() }} </p>
-        @endif
-
-        {{-- Concept --}}
-        @if(!str_contains($mot->typesString(), 'Chiffre'))
-           <p>Concept : {{$mot->syllabesString()}}</p>
-        @endif
-    </div>
+                    <h5>Concept</h5>
+                    <hr class="hr">
+                @if(!str_contains($mot->typesString(), 'Chiffre'))
+                    <p>{{$mot->syllabesString()}}</p>
+                @endif
+            </div>
+        </div>
+    </button>
 </div>
+
+
+<!--
+<div class="border border-1 border-dark rounded w-25 mx-1 my-3 bg-black bg-opacity-10">
+<div>
+<h2 class="text-center py-4 border-bottom text-primary bg-white border-top rounded">{{ $mot->enMacusi }}</h2>
+</div>
+<div class="px-4 lh-2 ">
+
+{{-- Traduction --}}
+<p class="fw-bold lead text-center">{{json_decode($mot->trads)->$locale}}</p>
+
+<p>
+{{-- Listing of type(s) --}}
+@if($mot->types->count() == 1)
+    <p>Type : {{ $mot->typesString() }} </p>
+@elseif($mot->types->count() == 2)
+    <p>Types : {{ $mot->typesString() }} </p>
+@endif
+
+{{-- Concept --}}
+@if(!str_contains($mot->typesString(), 'Chiffre'))
+    <p>Concept : {{$mot->syllabesString()}}</p>
+@endif
+</div>
+</div>
+-->
 
--- resources/views/dictionary/index.blade.php
+++ resources/views/dictionary/index.blade.php
@@ -1,18 +1,50 @@
 @extends('layouts.app')
+@php
+       $localesArray = ['FR' => 'Français', 'EN' => 'English', 'IT' => 'italiano', 'DE' => 'deutsch'];
+
+       $locale = strtoupper(app()->getLocale());
+
+       $localeString = $localesArray[$locale];
+
+       $count = 0;
+@endphp
 
 @section('content')
-    <div class="d-flex justify-content-center">
-        {{ $mots->links() }}
+    <h1 class="h3-title">Bienvenue sur le dictionnaire !</h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-9">
+                <p>Explorez notre vaste collection de mots. Téléchargez le PDF en un clic pour découvrir le pouvoir des mots et de notre incroyable dictionnaire en ligne MaCuSi dès maintenant !</p>
+            </div>
+            <div class="col-md-3 d-flex flex-column text-center">
+                <a href="" class="btn download py-3 px-4 border-0 rounded-pill bg-secondary text-white font-weight-bold align-baseline"><img src="{{Storage::url('img/icon-download.svg')}}" alt="Télécharger" height="23"> Télécharger en PDF</a>
+                <a href="" class="mt-2 text-secondary">Autres formats ici</a>
+            </div>
+        </div>
     </div>
-    <div class="d-flex flex-wrap justify-content-around ">
+
+    <div class="language-switch d-flex justify-content-center align-baseline">
+        <p>MaCuSi <img src="{{Storage::url('img/icon-simple-arrow.svg')}}"> {{$localeString}}</p>
+        <label class="switch">
+            <input type="checkbox">
+            <span class="slider round"></span>
+        </label>
+        <p>{{$localeString}} <img src="{{Storage::url('img/icon-simple-arrow.svg')}}"> MaCuSi</p>
+    </div>
+
+
+    <div class="d-flex flex-wrap justify-content-center" id="accordion">
         @foreach($mots as $mot)
             @if($mot->typesString() != 'Chiffre')
-                <x-card :mot="$mot"/>
+                <x-card :mot="$mot" :count="$count"/>
+                @php
+                    $count++;
+                @endphp
             @endif
         @endforeach
     </div>
     <div class="d-flex justify-content-center mt-4">
-         {{ $mots->links() }}
+        {{ $mots->links() }}
     </div>
 
 @endsection
