4e7484b2de2aead77886f297889abcbad6858e8e
Zal-com <st.guillaume@outlook.com>
2023-07-17 18:06:04 +0200

    fixed isAdmin function

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -65,7 +66,7 @@ class User extends Authenticatable
     }
 
     public function isAdmin() : bool {
-        return $this->status == 2;
+        return Auth::user()->hasRole('admin');
     }
 
     public function isActivated() : bool {
